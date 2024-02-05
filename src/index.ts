265f621f8dd2f74af7b1b696f680f9f2065dc2eb
Crysp <just.lyonya@gmail.com>
2024-02-05 13:35:47 +0400

    Initial commit

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,5 @@
-import { getInput, setOutput, error, info } from '@actions/core';
+import { getInput, setOutput, setFailed, error, info } from '@actions/core';
 import { exec } from '@actions/exec';
-import * as fs from 'fs';
 import fetch from 'node-fetch';
 
 const execCommand = async (command: string): Promise<string> => {
@@ -43,31 +42,14 @@ async function run() {
 
   setOutput('percent', percent);
 
-  info(
-    JSON.stringify({
-      JIRA_USER,
-      JIRA_API_TOKEN,
-      method: 'post',
-      headers: {
-        Authorization: 'Basic ' + Buffer.from(JIRA_USER + ':' + JIRA_API_TOKEN),
-        Accept: 'application/json',
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify({
-        metricSourceId:
-          'ari:cloud:compass:ba4bd631-12ed-4459-8a28-ee995fd1ee44:metric-source/68ae38a8-55f1-43d6-9a0c-7393f5dbd9d3/d34a60d1-a59a-45b3-b16a-172a95f19c07',
-        value: percent,
-        timestamp: new Date().toISOString(),
-      }),
-    }),
-  );
-
   const response = await fetch(
     'https://teem-co.atlassian.net/gateway/api/compass/v1/metrics',
     {
       method: 'post',
       headers: {
-        Authorization: 'Basic ' + Buffer.from(JIRA_USER + ':' + JIRA_API_TOKEN),
+        Authorization:
+          'Basic ' +
+          Buffer.from(JIRA_USER + ':' + JIRA_API_TOKEN).toString('base64'),
         Accept: 'application/json',
         'Content-Type': 'application/json',
       },
@@ -81,7 +63,7 @@ async function run() {
   );
 
   if (!response.ok) {
-    error(`Failed to post metric to JIRA: ${await response.text()}`);
+    setFailed(`Failed to post metric to JIRA: ${await response.text()}`);
   } else {
     info(await response.text());
   }
