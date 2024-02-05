0d7c7cb467d4deb6561487bc7d8eb7a39507562b
Crysp <just.lyonya@gmail.com>
2024-02-05 12:44:49 +0400

    Initial commit

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 import { getInput, setOutput, error, info } from '@actions/core';
 import { exec } from '@actions/exec';
 import * as fs from 'fs';
+import fetch from 'node-fetch';
 
 const execCommand = async (command: string): Promise<string> => {
   const output: string[] = [];
@@ -42,16 +43,22 @@ async function run() {
 
   setOutput('percent', percent);
 
-  await execCommand(`curl --request POST \\
---url https://teem-co.atlassian.net/gateway/api/compass/v1/metrics \\
---user "${JIRA_USER}:${JIRA_API_TOKEN}" \\
---header "Accept: application/json" \\
---header "Content-Type: application/json" \\
---data "{
-  \\"metricSourceId\\": \\"ari:cloud:compass:ba4bd631-12ed-4459-8a28-ee995fd1ee44:metric-source/68ae38a8-55f1-43d6-9a0c-7393f5dbd9d3/d34a60d1-a59a-45b3-b16a-172a95f19c07\\",
-  \\"value\\": ${percent},
-  \\"timestamp\\": \\"$(date -u +'%Y-%m-%dT%H:%M:%SZ')\\"
-}"`);
+  await fetch(
+    `https://${JIRA_USER}:${JIRA_API_TOKEN}@teem-co.atlassian.net/gateway/api/compass/v1/metrics`,
+    {
+      method: 'post',
+      headers: {
+        Accept: 'application/json',
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+        metricSourceId:
+          'ari:cloud:compass:ba4bd631-12ed-4459-8a28-ee995fd1ee44:metric-source/68ae38a8-55f1-43d6-9a0c-7393f5dbd9d3/d34a60d1-a59a-45b3-b16a-172a95f19c07',
+        value: percent,
+        timestamp: new Date().toISOString(),
+      }),
+    },
+  );
 }
 
 run();
